a0b3e6e60fb76d9ca05919d9e23ad0b8952ebb91
wuye <ye.wu@philips.com>
2021-06-09 11:44:00 +0800

    www

---
+++ www/www.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/go-redis/redis"
+)
+
+var rdb *redis.Client
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Received request from " + r.RemoteAddr)
+
+	count, err := rdb.Incr("counter").Result()
+
+	fmt.Fprintf(w, "pod: "+hostname+" at your service in "+strconv.FormatInt(count, 10)+" times !\n")
+}
+
+func main() {
+
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	pong, err := rdb.Ping().Result()
+	fmt.Println(pong, err)
+
+	http.HandleFunc("/", handler)
+	log.Println("Running...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
---
+++ www/www_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("aaaaaaa")
+	fmt.Println("bbbbbbb")
+	fmt.Println("ccccccc")
+	fmt.Println("ddddddd")
+
+}
